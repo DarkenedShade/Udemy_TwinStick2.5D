dce8e02735d1ac9b4b35e32f338cb35c008c796f
Junaid <messagejunaid@gmail.com>
2016-06-15 09:19:45 -0400

    Setup ReplaySystem

---
+++ Assets/ReplaySystem.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReplaySystem : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+public struct MyKeyFrame
+{
+    public float frameTime;
+    private Vector3 position;
+    private Quaternion rotation;
+
+    public MyKeyFrame(float time, Vector3 pos, Quaternion rot)
+    {
+        frameTime = time;
+        position = pos;
+        rotation = rot;
+    }
+}
